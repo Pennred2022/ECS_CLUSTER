4c978bf4555eac2ee39213d0fff8e1e207910332
Pennred2022 <94962749+Pennred2022@users.noreply.github.com>
2021-11-24 19:45:34 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+/*
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+} 
+*/
